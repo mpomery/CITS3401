a5b23750015a2edcaee36b441bc25dbb0339738e
mpomery <bob_george33@hotmail.com>
2014-04-26 04:57:37 +0800

    Fixed Newlines Properly

--- project1/prettify.py
+++ project1/prettify.py
@@ -1,6 +1,6 @@
 input = open('schema.xml', 'r')
 data = input.read()
-if "\r" not in data:
+if "\n" not in data:
 	output = open('schema.xml', 'w')
 	for line in data.split(">"):
 		if line is not "":
