6a48beae98c35dbf4d3e62f4a5c5235c58630dc6
mpomery <bob_george33@hotmail.com>
2014-04-26 04:55:48 +0800

    Fixed Prettify Script

--- project1/prettify.py
+++ project1/prettify.py
@@ -1,17 +1,12 @@
-
-
 input = open('schema.xml', 'r')
-
 data = input.read()
-
-
 if "\r" not in data:
 	output = open('schema.xml', 'w')
 	for line in data.split(">"):
 		if line is not "":
-			output.write(line + str(">\r"))
+			output.write(line + str(">\n"))
 		else:
-			output.write(str("\r"))
+			output.write(str("\n"))
 	output.close()
 else:
 	print("There are already newlines in this file")
