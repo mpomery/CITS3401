ceb8b2e9f88ef57c1b91faab5c45a78f13fbb7c6
mpomery <bob_george33@hotmail.com>
2014-06-02 18:32:46 +0800

    Submitted

---
+++ project2/submissionreceipt/cssubmit_files/CSM-SCROLLRSS.js
@@ -0,0 +1,45 @@
+
+/***********************************************
+* Cross browser Marquee II- (C) Dynamic Drive (www.dynamicdrive.com)
+* This notice MUST stay intact for legal use
+* Visit http://www.dynamicdrive.com/ for this script and 100s more.
+***********************************************/
+
+var delayb4scroll=1000 //initial delay in milliseconds
+var marqueespeed=1 //scroll speed (larger is faster 1-10)
+var pauseit=1 //Pause marquee onMousever (0=no. 1=yes)?
+
+////NO NEED TO EDIT BELOW THIS LINE////////////
+
+var copyspeed=marqueespeed
+var pausespeed=(pauseit==0)? copyspeed: 0
+var actualheight=''
+
+function scrollmarquee(){
+    if (parseInt(cross_marquee.style.top)>(actualheight*(-1)+8))
+	cross_marquee.style.top=parseInt(cross_marquee.style.top)-copyspeed+"px"
+    else
+	cross_marquee.style.top=parseInt(marqueeheight)+8+"px"
+}
+
+function initializemarquee(){
+    cross_marquee=document.getElementById("vmarquee")
+    cross_marquee.style.top=0
+    marqueeheight=document.getElementById("marqueecontainer").offsetHeight
+    actualheight=cross_marquee.offsetHeight
+    if (window.opera || navigator.userAgent.indexOf("Netscape/7")!=-1){
+//if Opera or Netscape 7x, add scrollbars to scroll and exit
+	cross_marquee.style.height=marqueeheight+"px"
+	cross_marquee.style.overflow="scroll"
+	return
+    }
+    setTimeout('lefttime=setInterval("scrollmarquee()",30)', delayb4scroll)
+}
+
+if (window.addEventListener)
+    window.addEventListener("load", initializemarquee, false)
+else if (window.attachEvent)
+    window.attachEvent("onload", initializemarquee)
+else if (document.getElementById)
+    window.onload=initializemarquee
+
